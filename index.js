13466e3654ccaf440c0ceba41f47dcc7b989ab24
Александр Серов <aleksandrserov@Aleksandrs-MacBook-Pro.local>
2021-07-21 21:28:03 +0300

    feat(*): Step I: The createElement Function

--- index.js
+++ index.js
@@ -1,18 +1,43 @@
-const element = {
-    type: "h1",
-    props: {
-        title: "foo",
-        children: "Hello",
-    },
+
+function createTextElement(text) {
+    return {
+        type: "TEXT_ELEMENT",
+        props: {
+            nodeValue: text,
+            children: [],
+        },
+    }
 }
 
+function createElement(type, props, ...children) {
+    return {
+        type,
+        props: {
+            ...props,
+            children: children.map((child) => {
+                typeof child === "object" ? child : createTextElement(child)
+            }),
+        },
+    }
+}
+
+function render(element, container) {
+    // TODO create dom nodes
+}
+
+const Didact = {
+    createElement,
+    render,
+};
+
+
+/** @jsx Didact.createElement */
+const element = (
+    <div id="foo">
+        <a>bar</a>
+        <b />
+    </div>
+)
+
 const container = document.getElementById("root")
-
-const node = document.createElement(element.type)
-node["title"] = element.props.title
-
-const text = document.createTextNode("")
-text["nodeValue"] = element.props.children
-
-node.appendChild(text)
-container.appendChild(node)
+Didact.render(element, container)
